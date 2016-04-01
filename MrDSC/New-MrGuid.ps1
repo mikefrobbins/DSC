85b6b75f6f7631d7c55ab345505bab8f5852e911
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-04-01 14:12:34 -0500

    Added comment based help to the New-MrGuid function

--- MrDSC/New-MrGuid.ps1
+++ MrDSC/New-MrGuid.ps1
@@ -1,4 +1,28 @@
 ﻿#Requires -Version 4.0
 function New-MrGuid {
+
+<#
+.SYNOPSIS
+    Creates a new GUID (globally unique identifier).
+ 
+.DESCRIPTION
+    The New-MrGuid function creates a new globally unique identifier which is a 128-bit integer number.
+
+.EXAMPLE
+     New-MrGuid
+ 
+.INPUTS
+    None
+ 
+.OUTPUTS
+    System.Guid
+ 
+.NOTES
+    Author:  Mike F Robbins
+    Website: http://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [Guid]::NewGuid()
+
 }
