1202ce6db0d4c125686cbc782b30bd6f2c22c55b
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-04-01 14:25:45 -0500

    Added comment based help to the Get-MrDSCResourceModulePath function

--- MrDSC/Get-MrDSCResourceModulePath.ps1
+++ MrDSC/Get-MrDSCResourceModulePath.ps1
@@ -1,6 +1,32 @@
 ﻿#Requires -Version 4.0
 function Get-MrDSCResourceModulePath {
 
+<#
+.SYNOPSIS
+    Returns path and version information for the root module of the specified DSC resource.
+ 
+.DESCRIPTION
+    The Get-MrDSCResourceModulePath function returns the name, module name, module version,
+    and module path for the root module of the specified DSC resource.
+ 
+.PARAMETER Name
+    DSC resource name.
+
+.EXAMPLE
+     Get-MrDSCResourceModulePath -Name Archive
+ 
+.INPUTS
+    None
+ 
+.OUTPUTS
+    PSCustomObject
+ 
+.NOTES
+    Author:  Mike F Robbins
+    Website: http://mikefrobbins.com
+    Twitter: @mikefrobbins
+#>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
