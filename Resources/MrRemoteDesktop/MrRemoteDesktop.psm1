451f3df94d38f083eb70d6509e36c1bb638aea1e
Mike F Robbins <mikefrobbins@users.noreply.github.com>
2016-09-30 15:11:13 -0500

    Eliminated switch statements and SetAuthSetting() & SetTSSetting() methods

--- Resources/MrRemoteDesktop/MrRemoteDesktop.psm1
+++ Resources/MrRemoteDesktop/MrRemoteDesktop.psm1
@@ -1,10 +1,10 @@
-﻿enum Ensure {
-    Absent
-    Present
+﻿enum UserAuthenication {
+    NonSecure = 0
+    Secure = 1
 }
-enum UserAuthenication {
-    NonSecure
-    Secure
+enum Ensure {
+    Present = 0
+    Absent = 1
 }
 
 [DscResource()]
@@ -28,11 +28,11 @@ class RemoteDesktop {
     [void]Set(){
 
         if ($this.TestAuthSetting() -eq $false) {
-            $this.SetAuthSetting($this.UserAuthenication)            
+            Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name 'UserAuthentication' -Value $this.UserAuthenication        
         }
 
         if ($this.TestTSSetting() -eq $false) {
-            $this.SetTSSetting($this.Ensure)            
+            Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name 'fDenyTSConnections' -Value $this.Ensure        
         }
 
     }
@@ -48,53 +48,21 @@ class RemoteDesktop {
 
     }
 
-    [string]GetAuthSetting(){
+    [UserAuthenication]GetAuthSetting(){
         
         $AuthCurrentSetting = Get-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name 'UserAuthentication' |
                               Select-Object -ExpandProperty UserAuthentication
 
-        $AuthSetting = switch ($AuthCurrentSetting) {
-            0 {'NonSecure'; Break}
-            1 {'Secure'; Break}
-        }
-
-	    Return $AuthSetting
+	    Return $AuthCurrentSetting
 
     }
 
-    [string]GetTSSetting(){
+    [Ensure]GetTSSetting(){
 
         $TSCurrentSetting = Get-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name 'fDenyTSConnections' |
                             Select-Object -ExpandProperty fDenyTSConnections
 
-        $TSSetting = switch ($TSCurrentSetting) {
-            0 {'Present'; Break}
-            1 {'Absent'; Break}
-        }
-
-	    Return $TSSetting
-
-    }
-
-    [void]SetAuthSetting([UserAuthenication]$UserAuthenication){
-
-        switch ($this.UserAuthenication) {
-            'NonSecure' {$Script:AuthDesiredSetting = 0; Break}
-            'Secure' {$Script:AuthDesiredSetting = 1; Break}
-        }
-
-        Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name 'UserAuthentication' -Value $Script:AuthDesiredSetting
-
-    }
-
-    [void]SetTSSetting([Ensure]$Ensure){
-
-        switch ($this.Ensure) {
-            'Present' {$Script:TSDesiredSetting = 0; Break}
-            'Absent' {$Script:TSDesiredSetting = 1; Break}
-        }
-
-        Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -Name 'fDenyTSConnections' -Value $Script:TSDesiredSetting
+	    Return $TSCurrentSetting
 
     }
 
